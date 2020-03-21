7635c2c27e82200bc3dee1a86e104321af9ca4df
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-03-21 18:31:34 +0000

    long shadow title

--- src/App.js
+++ src/App.js
@@ -4,28 +4,22 @@ import Logo from './img/JP-Logo-v1.png';
 
 function LongShadow(props) {
   return (
-    <div className="long-shadow">{props.text}</div>
+    <div id={props.id} className="long-shadow">{props.text}</div>
   )
 }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <LongShadow text={"Hello"}/>
-        <img className="App-logo" src={Logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hosted on Firebase.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <header className="app-header">
+          {/* <i class="fas fa-ellipsis-h fa-3x menu-icon"></i>
+          <img scr={Logo} alt="Logo"/> */}
+        </header>
+        <div className="app-body">
+          <div id="title-container">
+            <LongShadow id={"headline-title"} text={"Josh Pollard"}/>
+          </div>
+        </div>
     </div>
 
   );
