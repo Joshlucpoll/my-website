ee93b827c13a51bf431375b7b5c6ccde31d5fb71
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-04-13 01:17:17 +0100

    New animations + subtitle

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,59 @@
 import React from "react";
 import {isBrowser} from "react-device-detect";
+import styled, {keyframes, ThemeConsumer} from "styled-components";
+import {fadeInDownBig} from "react-animations";
 
 // Components
 import LongShadow from "../components/longShadow";
 
 import "../styles/home.scss";
 
+const FadeIn = styled.div`animation: 2s ${keyframes`${fadeInDownBig}`}`;
+const string = "<Software-Developer/>";
+let i = 0, howManyTimes = 21;
+
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subTitle: "",
+      cursor: "",
+    }
+  }
+  
+  typeSubTitle() {
+    this.setState((state) => ({
+      subTitle: state.subTitle + string.split("")[i] 
+    }));
+    i++;
+    if( i < howManyTimes ){
+      setTimeout(() => {
+        this.typeSubTitle();
+      }, 100);
+
+    }
+  }
+
+  subTitleCursor() {
+    if (this.state.cursor === "") {
+      this.setState({ cursor: "|"});
+    } else {
+      this.setState({ cursor: ""});
+    }
+    setTimeout(() => {
+      this.subTitleCursor();
+    }, 530);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.subTitleCursor();
+    }, 2000);
+
+    setTimeout(() => {
+      this.typeSubTitle();
+    }, 4000);
+  }
 
   render() {
     let xVector = this.props.xMiddle - this.props.xMouse;
@@ -22,9 +69,11 @@ class Home extends React.Component {
     else {
       styles = {}
     }
+
     return (
       <div className="home-body">
-        <div
+        
+        <FadeIn><div
           id="title-container"
           style={styles}
         >
@@ -35,7 +84,8 @@ class Home extends React.Component {
             xVector={xVector}
             yVector={yVector}
           />
-        </div>
+          <div className="sub-title">{this.state.subTitle}{this.state.cursor}</div>
+        </div></FadeIn>
       </div>
     );
   }
