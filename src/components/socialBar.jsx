04893ab1ec808d70f3618679acb93f8eedaf8a2e
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-05-05 12:26:18 +0100

    Changed social bar to functional component

--- src/components/socialBar.jsx
+++ src/components/socialBar.jsx
@@ -8,39 +8,38 @@ import Email from "../assets/social_icons/gmail.svg";
 
 import "../styles/socialBar.scss";
 
-class SocialBar extends React.Component {
-  render() {
-    return (
-      <div className="social-bar-container"> 
-          <a href="https://github.com/joshlucpoll" target="_blank" rel="noopener noreferrer">
-            <div className="github">
-              <img src={Github} alt="Github"/>
-            </div>
-          </a>
-          <a href="https://twitter.com/Joshlucpoll" target="_blank" rel="noopener noreferrer">
-            <div className="twitter">
-              <img src={Twitter} alt="Twitter"/>
-            </div>
-          </a>
-          <a href="https://stackoverflow.com/users/10472451/joshlucpoll" target="_blank" rel="noopener noreferrer">
-            <div className="stackoverflow">
-              <img src={StackOverflow} alt="StackOverflow"/>
-            </div>
-          </a>
-          <a href="https://www.linkedin.com/in/joshlucpoll" target="_blank" rel="noopener noreferrer">
-            <div className="linkedin">
-              <img src={LinkedIn} alt="CodeSandbox"/>
-            </div>
-          </a>
-          <a href="mailto:info@joshlucpoll.com" target="_blank" rel="noopener noreferrer">
-            <div className="email">
-              <img src={Email} alt="Email"/>
-            </div>
-          </a>
-      </div>
-    );
-  }
+function SocialBar() {
+  return (
+    <div className="social-bar-container"> 
+        <a href="https://github.com/joshlucpoll" target="_blank" rel="noopener noreferrer">
+          <div className="github">
+            <img src={Github} alt="Github"/>
+          </div>
+        </a>
+        <a href="https://twitter.com/Joshlucpoll" target="_blank" rel="noopener noreferrer">
+          <div className="twitter">
+            <img src={Twitter} alt="Twitter"/>
+          </div>
+        </a>
+        <a href="https://stackoverflow.com/users/10472451/joshlucpoll" target="_blank" rel="noopener noreferrer">
+          <div className="stackoverflow">
+            <img src={StackOverflow} alt="StackOverflow"/>
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/joshlucpoll" target="_blank" rel="noopener noreferrer">
+          <div className="linkedin">
+            <img src={LinkedIn} alt="CodeSandbox"/>
+          </div>
+        </a>
+        <a href="mailto:info@joshlucpoll.com" target="_blank" rel="noopener noreferrer">
+          <div className="email">
+            <img src={Email} alt="Email"/>
+          </div>
+        </a>
+    </div>
+  );
 }
+
 
 
 
