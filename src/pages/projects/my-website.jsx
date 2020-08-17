df009ffa230a2084b2f4a4daf0f6faa347609e7c
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-08-17 12:18:44 +0100

    Bug fix

--- src/pages/projects/my-website.jsx
+++ src/pages/projects/my-website.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { motion } from "framer-motion";
-import { pageTransition } from "../../styles/pageTransition";
 
 import "../../styles/projects/project.scss";
 
